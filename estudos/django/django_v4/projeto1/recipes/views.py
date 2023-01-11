af23573fd84c20f31e0a63148bcdc460dbec9b04
pedropanosso@gmail.com <pedropanosso@gmail.com>
2023-01-11 12:28:00 -0300

    Create factory with fake data for tests

--- estudos/django/django_v4/projeto1/recipes/views.py
+++ estudos/django/django_v4/projeto1/recipes/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 
+from .utils.factory import make_recipe
+
 
 # Create your views here.
 def index_page(request):
     return render(request, 'recipes/templates/pages/home.html',
-                  {'name': 'Pedro Machado'})
+                  context={
+                      'recipes': [make_recipe for _ in range(10)]
+                  })
 
 
 def recipe_view(request, recipe_id):
     return render(request, 'recipes/templates/pages/recipe-view.html',
-                  {'name': recipe_id})
+                  context={
+                      'recipe': make_recipe
+                  })
