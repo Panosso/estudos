d78e865f4aee5b82edba0e214578b4536ee2bc15
pedropanosso@gmail.com <pedropanosso@gmail.com>
2023-04-22 11:37:51 -0300

    Create test files

--- estudos/django/django_v4/projeto1/recipes/tests.py
+++ estudos/django/django_v4/projeto1/recipes/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class RecipeURLsTest(TestCase):
+    # Cria uma função que vai testar se os testes
+    # que foram configurados no pytest estão OK
+    def test_the_pytest_is_ok(self):
+        ...
