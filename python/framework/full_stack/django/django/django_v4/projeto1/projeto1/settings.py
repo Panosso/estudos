bd383f4b450e2b57860a8363f330fcf2b27fea7c
Pedro Panosso Machado <pedropanosso@gmail.com>
2024-12-14 18:18:04 -0300

    Finish Cap14

--- python/framework/full_stack/django/django/django_v4/projeto1/projeto1/settings.py
+++ python/framework/full_stack/django/django/django_v4/projeto1/projeto1/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-w!yxgi%r)-g=\
-                !r67yz+*jto*rumj%51580fkamg#%p=q_u1@##"
+SECRET_KEY = f"django-insecure-w!yxgi%r)-g=!r67yz+*jto*rumj%51580fkamg#%p=q_u1@##"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
