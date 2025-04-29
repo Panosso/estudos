c55925594bb447fc5df9b0f89da37534cbdfca84
Pedro Panosso Machado <pedropanosso@gmail.com>
2025-04-29 00:05:32 -0300

    Add unique order challenge text

--- python/challenges/unique_order_done/unique_in_order.py
+++ python/challenges/unique_order_done/unique_in_order.py
@@ -1,3 +1,7 @@
+# In this challenge you need to create a function that return a list with 1 character from a list.
+# for example:
+#   ['AAAABBBBCCCCccDDEEEeAA'] = ['A', 'B', 'C', 'c', 'D', 'E', 'e', 'A']
+
 def unique_in_order(sequence):
 
     unique_order = []
@@ -10,8 +14,8 @@ def unique_in_order(sequence):
 
     return unique_order
     
-unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
-unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
-unique_in_order([1, 2, 2, 3, 3])   == [1, 2, 3]
-unique_in_order((1, 2, 2, 3, 3))   == [1, 2, 3]
-unique_in_order('AAAABBBCCDAABBB') == ["A", "B", "C", "D", "A", "B"]
+print(unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B'])
+print(unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D'])
+print(unique_in_order([1, 2, 2, 3, 3])   == [1, 2, 3])
+print(unique_in_order((1, 2, 2, 3, 3))   == [1, 2, 3])
+print(unique_in_order('AAAABBBCCDAABBB') == ["A", "B", "C", "D", "A", "B"])
