f39daac601d81e36507db469c1a68668211894c9
Pedro Panosso <pedropanosso@gmail.com>
2026-03-23 20:47:22 -0300

    Refactory the git repository

---
+++ JavaScript/language/js/curso_js/anotacoes.md
@@ -0,0 +1,4 @@
+Instalado o live server que sera usado para envio de request e response
+
+F.lux -> App que trabalha com a luminosidade da tela.
+
---
+++ JavaScript/language/js/curso_js/etapa_01/aula_02/app.js
@@ -0,0 +1,21 @@
+//strings
+
+const email = "pedropanosso@gmail.com"
+
+console.log(email)
+
+//concatenação de string
+
+const firstName = 'Pedro'
+const lastName = 'Machado'
+
+const fullname = firstName + ' ' + lastName
+
+console.log(fullname)
+
+console.log(fullname[0])
+
+//Metodos de string, e como todos os métodos precisam ser invocados, com () passando ou não argumentos.
+
+//deixa tudo maiusculo
+console.log(fullname.toUpperCase())
